84cde2354eb8f29185c335787717aac149e69006
Luka Jaredic <luka.jaredic@effectiva.hr>
2026-08-05 17:01:26 +0200

    add step 2

--- src/multi-step-form/assets/scripts/index.js
+++ src/multi-step-form/assets/scripts/index.js
@@ -27,21 +27,36 @@ import { validateForm } from "./validation";
 
 function registerFormEventHandlers() {
   const steps = [
-    1,
-    // 2, 3, 4
+    1, 2,
+    // 3, 4
   ];
 
   steps.forEach((step) => {
     const formElement = document.getElementById(`step-${step}-form`);
+    // Indicator looks clickable, so we will add click listener to it
+    const toggleBillingTypeIndicatorElement = formElement.querySelector(
+      ".billing-type__indicator",
+    );
 
     // Remove possible stale handlers
     formElement.removeEventListener("submit", handleSubmit);
+    formElement.removeEventListener("input", handleBillingTypeChange);
     formElement.removeEventListener("input", handleFormInput);
+    toggleBillingTypeIndicatorElement?.removeEventListener(
+      "click",
+      handleBillingTypeToggle,
+    );
 
     // Only register handlers for the current step
     if (step === formState.currentStepIndex) {
       formElement.addEventListener("submit", handleSubmit);
+      formElement.addEventListener("input", handleBillingTypeChange);
       formElement.addEventListener("input", handleFormInput);
+      formElement;
+      toggleBillingTypeIndicatorElement?.addEventListener(
+        "click",
+        handleBillingTypeToggle,
+      );
       submitButtonElement.setAttribute("form", formElement.id);
     }
   });
@@ -49,7 +64,7 @@ function registerFormEventHandlers() {
 
 function handleFormInput(event) {
   // Don't validate non-submitted forms
-  if (!formState.isCurrentFormSubmitted) return;
+  if (!formState.isCurrentFormSubmitted) return true;
 
   if (validateForm(event.currentTarget)) {
     submitButtonElement.disabled = false;
@@ -60,6 +75,7 @@ function handleFormInput(event) {
 
 function handleSubmit(event) {
   event.preventDefault();
+
   const formElement = event.target;
   formState.isCurrentFormSubmitted = true;
 
@@ -78,6 +94,33 @@ function handleSubmit(event) {
   };
 
   handleStepChange(formState.currentStepIndex, formState.currentStepIndex + 1);
+}
+
+function handleBillingTypeChange(event) {
+  if (!(event.target.name === "billing-type")) return;
+  const nextBillingValue = event.target.value;
+
+  document
+    .querySelectorAll("[data-type-yearly-content][data-type-monthly-content]")
+    .forEach((element) => {
+      element.textContent = element.getAttribute(
+        `data-type-${nextBillingValue}-content`,
+      );
+    });
+}
+
+function handleBillingTypeToggle(event) {
+  const formElement = event.currentTarget.closest("form");
+  const currentValue = formElement.querySelector(
+    'input[name="billing-type"]:checked',
+  ).value;
+  const nextValue = currentValue === "monthly" ? "yearly" : "monthly";
+  const nextValueElement = formElement.querySelector(
+    `input[name="billing-type"][value="${nextValue}"]`,
+  );
+
+  nextValueElement.checked = true;
+  nextValueElement.dispatchEvent(new Event("input", { bubbles: true }));
 }
 
 function handleStepChange(oldStep, newStep) {
